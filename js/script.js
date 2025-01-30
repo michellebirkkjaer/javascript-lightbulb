f6a920179b2c7a926791b178cc66a3f86b1374cf
michellebirkkjaer <michelle.birkkjaer@hotmail.dk>
2025-01-30 12:22:34 +0100

    færdig

--- js/script.js
+++ js/script.js
@@ -9,3 +9,11 @@ function onBulb() {
     const bulb = document.getElementById("bulb");
     bulb.src = "img/off.jpg";
   }
+
+
+  //Man kunne også tage fat i id'et i vores html og skrive:
+  //document.getElementById("onBtn").addEventListener('click', onBulb);
+  //addEventListener er en lytter (hvad vil du lytte til)til click
+
+  //eler:
+  //  //document.getElementById("offBtn").addEventListener('click', offBulb);
