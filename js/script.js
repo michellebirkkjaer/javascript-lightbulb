948cac77e0354b4be7a2a4bdf26aac2e917621bd
michellebirkkjaer <michelle.birkkjaer@hotmail.dk>
2025-01-30 12:13:40 +0100

    færdig

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function onBulb() {
+    const bulb = document.getElementById("bulb");
+    bulb.src = "img/on.jpg";
+  }
+  
+  function offBulb() {
+    const bulb = document.getElementById("bulb");
+    bulb.src = "img/off.jpg";
+  }
